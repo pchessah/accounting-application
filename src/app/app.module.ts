4fe1aeb4c45e3e1c6c5b7665295ffccb526c6f94
Paul Chesa <paulchesa1@gmail.com>
2021-05-04 16:12:50 +0300

    started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 import { AppRoutingModule } from './app-routing.module';
+
+import {MatCardModule} from '@angular/material/card';
+
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+
+const MATMODULES = [MatCardModule]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    ...MATMODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
