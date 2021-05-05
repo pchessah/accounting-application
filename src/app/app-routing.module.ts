e960e4798d1cc44bc4cc1456c7ef7d79c2bda5d0
Paul Chesa <paulchesa1@gmail.com>
2021-05-05 19:51:17 +0300

    Finished design of login and sign up page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { SignupComponent } from "../app/components/signup/signup.component";
-import {  LoginComponent  } from "../app/components/login/login.component";
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { SignupComponent } from '../app/components/signup/signup.component'
+import { LoginComponent } from '../app/components/login/login.component'
 
 const routes: Routes = [
   { path: 'sign-up', component: SignupComponent },
   { path: 'log-in', component: LoginComponent },
-];
+  { path: '', component: SignupComponent },
+ 
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
