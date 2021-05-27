e9bcf607a87f23b3c665f60b027909688ebecc30
Paul Chesa <paulchesa1@gmail.com>
2021-05-27 15:20:05 +0300

    Sales report component

--- src/app/storeowner/sales/sales-report/sales-report.component.ts
+++ src/app/storeowner/sales/sales-report/sales-report.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { IproductData } from 'src/app/libs/interfaces/IproductData';
+import { ViewSalesReportComponent } from '../view-sales-report/view-sales-report.component';
 
 @Component({
   selector: 'app-sales-report',
@@ -7,7 +14,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesReportComponent implements OnInit {
 
-  constructor() { }
+  date: any;
+
+  singleProduct: IproductData = {
+    stock_name: '',
+    quantity: '',
+    buying_price: '',
+    selling_price: '',
+  }
+
+  displayedColumns: string[] = [
+    'stock_name',
+    'quantity',
+    'selling_price',
+  ]
+  productData: IproductData[] = [
+    {
+      stock_name: 'Guiness 500ml',
+      quantity: '2',
+      buying_price: '150',
+      selling_price: '220',
+    },
+    {
+      stock_name: 'Tusker 500ml',
+      quantity: '1',
+      buying_price: '150',
+      selling_price: '200',
+    },
+  ]
+  dataSource: MatTableDataSource<IproductData>
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+  @ViewChild(MatSort) sort!: MatSort
+
+  constructor(public dialog: MatDialog, public router: Router) {
+    this.dataSource = new MatTableDataSource(this.productData)
+    this.getCurrentDate()
+    
+  }
+  goToMainDashboard(){
+    this.router.navigateByUrl("/dashboard")
+  }
+
+  getCurrentDate(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    this.date = mm + '/' + dd + '/' + yyyy;
+  }
+
+  pickDate(){
+    const dialogRef = this.dialog.open(ViewSalesReportComponent, {
+      width: '400px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.singleProduct = result;
+    });
+
+  }
 
   ngOnInit(): void {
   }
